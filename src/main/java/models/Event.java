d9eb073f673d29a9e72f7c83656fbf1797750eb5
Kate Trahan <kathrynceciliatrahan@gmail.com>
2018-01-09 17:08:47 -0800

    add README

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,12 +1,12 @@
 package models;
 
-public class Event{
+public class Event {
     private String people;
     private String food;
     private String booze;
     private String entertainment;
 
-    public Event (String people, String food, String booze, String entertainment){
+    public Event(String people, String food, String booze, String entertainment) {
         this.people = people;
         this.food = food;
         this.booze = booze;
@@ -14,9 +14,9 @@ public class Event{
 
     }
 
-    public Integer getPeople(Event people){
+    public Integer getPeople(Event people) {
         Integer cost = 0;
-        if (people.people.equals("people")){
+        if (people.people.equals("people")) {
             cost = 100;
         } else {
             cost = 0;
@@ -24,9 +24,9 @@ public class Event{
         return cost;
     }
 
-    public Integer getFood(Event food){
+    public Integer getFood(Event food) {
         Integer costFood = 0;
-        if (food.food.equals("food")){
+        if (food.food.equals("food")) {
             costFood = 100;
         } else {
             costFood = 0;
@@ -35,18 +35,24 @@ public class Event{
 
     }
 
-    public Integer getBooze(Event booze){
+    public Integer getBooze(Event booze) {
         Integer costBooze = 0;
         if (booze.booze.equals("booze")) {
             costBooze = 100;
         } else {
-                costBooze = 0;
+            costBooze = 0;
         }
         return costBooze;
+    }
+
+
+    public String getEntertainment(Event entertainment) {
+        Integer costBand = 0;
+        if (entertainment.entertainment.equals("band")) {
+            costBand = 100;
+        } else {
+            costBand = 0;
         }
-
-
-    public String getEntertainment(){
-        return "";
+        return costBand;
     }
 }
